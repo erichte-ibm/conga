f3f82f4bd9549260415e52bd6cc4cc4aad943494
Nick Child <nick.child@ibm.com>
2021-12-10 12:42:42 -0500

    Return vector of regex matches instead of one string
    Previously, only one match was returned by the command parsing function.
    Now, return a vector of all strings that matched the regex.
    Additionally, the return value has been changed from an Option to a Result.
    This allows for easier propagation of errors.
    All functions have been changed to use the new command parsing function
    correctly.
    
    Signed-off-by: Nick Child <nick.child@ibm.com>

--- src/cpu.rs
+++ src/cpu.rs
@@ -3,8 +3,6 @@
 use crate::collector::*;
 
 pub fn get_cores(col: &mut Collector) -> Result<CollectorValue, CollectorErr> {
-    match col.get_regex_from_command("lscpu", r"\s+Core\(s\) per socket:\s+(\d+)") {
-        None => Err("Failed to find regex capture".into()),
-        Some(capture) => Ok(CollectorValue::Integer(capture.parse::<i64>().unwrap()))
-    }
+    let captures = col.parse_from_command("lscpu", r"\s+Core\(s\) per socket:\s+(\d+)")?;
+    Ok(CollectorValue::Integer(captures[1].parse::<i64>().unwrap()))
 }
--- src/memory.rs
+++ src/memory.rs
@@ -8,66 +8,48 @@ const MEM_COMMAND: &'static str = "free";
 
 
 pub fn get_mem_total(col: &mut Collector) -> Result<CollectorValue, CollectorErr> {
-    match col.get_regex_from_command(MEM_COMMAND, r"\s+Mem:\s+(\d+)") {
-        None => Err("Failed to find regex capture".into()),
-        Some(capture) => Ok(CollectorValue::Integer(capture.parse::<i64>().unwrap()))
-    }
+    let captures = col.parse_from_command(MEM_COMMAND, r"\s+Mem:\s+(\d+)")?;
+    Ok(CollectorValue::Integer(captures[1].parse::<i64>().unwrap()))
 }
 
 pub fn get_mem_used(col: &mut Collector) -> Result<CollectorValue, CollectorErr> {
-    match col.get_regex_from_command(MEM_COMMAND,r"\s+Mem:\s+\d+\s+(\d+)") {
-        None => Err("Failed to find regex capture".into()),
-        Some(capture) => Ok(CollectorValue::Integer(capture.parse::<i64>().unwrap()))
-    }
+    let captures = col.parse_from_command(MEM_COMMAND,r"\s+Mem:\s+\d+\s+(\d+)")?;
+    Ok(CollectorValue::Integer(captures[1].parse::<i64>().unwrap()))
 }
 
 pub fn get_mem_free(col: &mut Collector) -> Result<CollectorValue, CollectorErr> {
-    match col.get_regex_from_command(MEM_COMMAND, r"\s+Mem:\s+(?:\d+\s+){2}(\d+)") {
-        None => Err("Failed to find regex capture".into()),
-        Some(capture) => Ok(CollectorValue::Integer(capture.parse::<i64>().unwrap()))
-    }
+    let captures = col.parse_from_command(MEM_COMMAND,r"\s+Mem:\s+(?:\d+\s+){2}(\d+)")?;
+    Ok(CollectorValue::Integer(captures[1].parse::<i64>().unwrap()))
 }
 
 pub fn get_mem_shared(col: &mut Collector) -> Result<CollectorValue, CollectorErr> {
-    match col.get_regex_from_command(MEM_COMMAND, r"\s+Mem:\s+(?:\d+\s+){3}(\d+)") {
-        None => Err("Failed to find regex capture".into()),
-        Some(capture) => Ok(CollectorValue::Integer(capture.parse::<i64>().unwrap()))
-    }
+    let captures = col.parse_from_command(MEM_COMMAND,r"\s+Mem:\s+(?:\d+\s+){3}(\d+)")?;
+    Ok(CollectorValue::Integer(captures[1].parse::<i64>().unwrap()))
 }
 
 pub fn get_mem_buff_and_cache(col: &mut Collector) -> Result<CollectorValue, CollectorErr> {
-    match col.get_regex_from_command(MEM_COMMAND, r"\s+Mem:\s+(?:\d+\s+){4}(\d+)") {
-        None => Err("Failed to find regex capture".into()),
-        Some(capture) => Ok(CollectorValue::Integer(capture.parse::<i64>().unwrap()))
-    }
+    let captures = col.parse_from_command(MEM_COMMAND, r"\s+Mem:\s+(?:\d+\s+){4}(\d+)")?;
+    Ok(CollectorValue::Integer(captures[1].parse::<i64>().unwrap()))
 }
 
 pub fn get_mem_available(col: &mut Collector) -> Result<CollectorValue, CollectorErr> {
-    match col.get_regex_from_command(MEM_COMMAND, r"\s+Mem:\s+(?:\d+\s+){5}(\d+)") {
-        None => Err("Failed to find regex capture".into()),
-        Some(capture) => Ok(CollectorValue::Integer(capture.parse::<i64>().unwrap()))
-    }
+    let captures = col.parse_from_command(MEM_COMMAND,r"\s+Mem:\s+(?:\d+\s+){5}(\d+)")?;
+    Ok(CollectorValue::Integer(captures[1].parse::<i64>().unwrap()))
 }
 
 // Do similar logic for swap values
 
 pub fn get_swap_total(col: &mut Collector) -> Result<CollectorValue, CollectorErr> {
-    match col.get_regex_from_command(MEM_COMMAND, r"\s+Swap:\s+(\d+)") {
-        None => Err("Failed to find regex capture".into()),
-        Some(capture) => Ok(CollectorValue::Integer(capture.parse::<i64>().unwrap()))
-    }
+    let captures = col.parse_from_command(MEM_COMMAND,r"\s+Swap:\s+(\d+)")?;
+    Ok(CollectorValue::Integer(captures[1].parse::<i64>().unwrap()))
 }
 
 pub fn get_swap_used(col: &mut Collector) -> Result<CollectorValue, CollectorErr> {
-    match col.get_regex_from_command(MEM_COMMAND, r"\s+Swap:\s+\d+\s+(\d+)") {
-        None => Err("Failed to find regex capture".into()),
-        Some(capture) => Ok(CollectorValue::Integer(capture.parse::<i64>().unwrap()))
-    }
+    let captures = col.parse_from_command(MEM_COMMAND,r"\s+Swap:\s+\d+\s+(\d+)")?;
+    Ok(CollectorValue::Integer(captures[1].parse::<i64>().unwrap()))
 }
 
 pub fn get_swap_free(col: &mut Collector) -> Result<CollectorValue, CollectorErr> {
-    match col.get_regex_from_command(MEM_COMMAND, r"\s+Swap:\s+(?:\d+\s+){2}(\d+)") {
-        None => Err("Failed to find regex capture".into()),
-        Some(capture) => Ok(CollectorValue::Integer(capture.parse::<i64>().unwrap()))
-    }
+    let captures = col.parse_from_command(MEM_COMMAND,r"\s+Swap:\s+(?:\d+\s+){2}(\d+)")?;
+    Ok(CollectorValue::Integer(captures[1].parse::<i64>().unwrap()))
 }
