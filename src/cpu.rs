ac8ae1a541844ea007485ad5142f61cbed3799c2
Eric Richter <erichte@linux.ibm.com>
2022-05-06 15:32:59 -0500

    collector: use lazy_regex to check regex validity at compile-time
    A static regex should *never* fail at runtime. The lazy_regex crate provides
    a `regex!()` macro that builds a Regex struct, and checks its validity at
    compile-time. This should aid development of new collectors, as these
    failures can now be spotted at development time, and potentially even
    show up in-editor.
    
    As a side effect, `parse_from_command` now expects a Regex struct rather
    than a string. In all cases where a regex is not generated at runtime, this
    can (and has been) replaced with a call to `regex!()` in the same argument line.
    
    For example, this collector line:
    
      let captures = col.parse_from_command("lscpu", r"\s+Core\(s\) per socket:\s+(\d+)", OutputStream::STDOUT)?;
    
    can be now written as:
    
      let captures = col.parse_from_command("lscpu", regex!(r"\s+Core\(s\) per socket:\s+(\d+)"), OutputStream::STDOUT)?;
    
    At the time of this commit, no collector generates their regex string at
    runtime, all are static strings.

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright 2021 IBM Corp.
 use crate::collector::*;
+use lazy_regex::regex;
+
 
 pub fn get_cores(col: &mut Collector) -> Result<CollectorValue, CollectorErr> {
-    let captures = col.parse_from_command("lscpu", r"\s+Core\(s\) per socket:\s+(\d+)", OutputStream::STDOUT)?;
+    let captures = col.parse_from_command("lscpu", regex!(r"\s+Core\(s\) per socket:\s+(\d+)"), OutputStream::STDOUT)?;
     Ok(CollectorValue::Integer(captures[1].parse::<i64>().unwrap()))
 }
--- src/os.rs
+++ src/os.rs
@@ -2,17 +2,18 @@
 // Copyright 2021 IBM Corp.
 
 use crate::collector::*;
+use lazy_regex::regex;
 
 const KERNEL_RELEASE_COMMAND: &'static str = "uname --kernel-release";
 const OS_NAME_COMMAND: &'static str = "grep PRETTY_NAME /etc/os-release";
 
 pub fn get_kernel_release(col: &mut Collector) -> Result<CollectorValue, CollectorErr> {
-    let captures = col.parse_from_command(KERNEL_RELEASE_COMMAND, r"(.*)", OutputStream::STDOUT)?;
+    let captures = col.parse_from_command(KERNEL_RELEASE_COMMAND, regex!(r"(.*)"), OutputStream::STDOUT)?;
     Ok(CollectorValue::Text(captures[1].to_string()))
 }
 
 
 pub fn get_os_name(col: &mut Collector) -> Result<CollectorValue, CollectorErr> {
-    let captures = col.parse_from_command(OS_NAME_COMMAND, r"PRETTY_NAME=.(.*).", OutputStream::STDOUT)?;
+    let captures = col.parse_from_command(OS_NAME_COMMAND, regex!(r"PRETTY_NAME=.(.*)."), OutputStream::STDOUT)?;
         Ok(CollectorValue::Text(captures[1].to_string()))
 }
